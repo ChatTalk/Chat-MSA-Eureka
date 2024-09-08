d278c14d09ca070fb7e33f96bf2f86fc72283720
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-08 18:55:09 +0900

    initial setting

---
+++ src/main/java/com/example/chatservereureka/ChatServerEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatservereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatservereureka/ChatServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
